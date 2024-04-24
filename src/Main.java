00f446da020fc5274195b15cd0d0c37e9104041b
BehzodFarkhodov <behzodfarhodov13@gmail.com>
2024-04-24 12:11:10 +0500

    add task 4 from branch task4

--- src/Main.java
+++ src/Main.java
@@ -1,17 +1,51 @@
-// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
-// then press Enter. You can now see whitespace characters in your code.
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Set;
+
 public class Main {
-    public static void main(String[] args) {
-        // Press Alt+Enter with your caret at the highlighted text to see how
-        // IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+        public static void main(String[] args) {
 
-        // Press Shift+F10 or click the green arrow button in the gutter to run the code.
-        for (int i = 1; i <= 5; i++) {
+                Set<String> set = new HashSet<>();
+                Set<String> set2 = new HashSet<>();
+                Set<String> set3 = new HashSet<>();
+                set.add("olma");
+                set.add("behi");
+                set.add("nok");
+                set.add("tuxum");
+                set.add("guruch");
 
-            // Press Shift+F9 to start debugging your code. We have set one breakpoint
-            // for you, but you can always add more by pressing Ctrl+F8.
-            System.out.println("i = " + i);
+                set2.add("behi");
+                set2.add("nok");
+                set2.add("test");
+                set2.add("shaftoli");
+                set2.add("tuxum");
+
+                set3.add("susambl");
+                set3.add("daraxt");
+                set3.add("nok");
+                set3.add("guruch");
+                set3.add("olma");
+
+                Set<String> result = mySet(set, set2, set3);
+                System.out.println(result);
         }
-    }
+
+        public static Set<String> mySet(Set<String> set, Set<String> set2, Set<String> set3) {
+                Set<String> newSet = new HashSet<>();
+                newSet.addAll(set);
+                newSet.addAll(set2);
+//                newSet.addAll(set3);
+                for (String str : set3) {
+                        if (!set.contains(str) && !set2.contains(str)) {
+                               newSet.add(str);
+                        }
+                }
+
+                return newSet;
+        }
 }
+
+
+
+
+
